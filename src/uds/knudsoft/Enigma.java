ef893cc67e2a64a199b1960bda9b9c6bbc390678
knud21kea <grah0070@stud.kea.dk>
2021-09-15 17:02:03 +0200

    Enigma with: UI Encode number

--- src/uds/knudsoft/Enigma.java
+++ src/uds/knudsoft/Enigma.java
@@ -64,7 +64,21 @@ public class Enigma {
     //Encode Number
     public static void encodeNumberCipher() {
         System.out.println("Encoding with Number Cipher...");
-        System.out.println("Koden er: {1, 2, 5}");
+        //input a string converted to upper case - test with "abe"
+        System.out.print("Indtast en streng: ");
+        String inputString = input.nextLine();
+        inputString = inputString.toUpperCase();
+
+        //convert the input string to a list of numbers
+        int[] intArray = stringToNumbers(inputString);
+
+        //Could shift here for Caesar
+
+        //convert the list of numbers to a formatted string of codes
+        String outputString = convertListOfNumbersToString(intArray);
+
+        //output encoded string
+        System.out.println("Koden er: " + outputString);
     }
 
     //Decode Number
@@ -97,7 +111,81 @@ public class Enigma {
         System.out.println("Teksten var: ABE");
     }
 
+    //method to convert a string to a list of numbers
+    public static int[] stringToNumbers(String inputString) {
+        int inputLength = inputString.length();
+        int[] intArray = new int[inputLength];
+        String letter;
+        for (int i = 0; i < inputLength; i++) {
+            letter = inputString.substring(i, i + 1);
+            int index = letterToInt(letter);
+            intArray[i] = index;
+        }
+        return intArray;
+    }
+
+    //method to convert 1 letter to a number
+    public static int letterToInt(String letter) {
+        final String CHARACTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        return CHARACTERS.indexOf(letter);
+    }
+
+    //method to convert a list of numbers to a string
+    public static String convertListOfNumbersToString(int[] intArray) {
+        int inputLength = intArray.length;
+        String outputString = "{";
+        for (int i = 0; i < inputLength - 1; i++) {
+            outputString += intArray[i] + ", "; //todo research StringBuilder
+        }
+        outputString += intArray[inputLength - 1] + "}";
+        return outputString;
+    }
+
+    //method to convert 1 number to a letter
+    public static String convertNumberToLetter(int number) {
+        final String CHARACTERS = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
+        return CHARACTERS.substring(number, number + 1);
+    }
+
+    //find how many elements = number of commas +1
+    public static int getNumberOfElements(String inputString) {
+        int firstComma = inputString.indexOf(",");
+        int nextComma = 0;
+        int lastComma = inputString.lastIndexOf(",", firstComma + 1);
+        int count = -1;
+        while (nextComma != lastComma) {
+            count++;
+            nextComma = inputString.indexOf(",", firstComma + 1);
+            firstComma = nextComma;
+        }
+        return count + 1;
+    }
+
+    //extract code strings from input string
+    public static String[] sliceStringIntoCodeStrings(String inputString, int codes) {
+        String[] codeString = new String[codes];
+        for (int i = 0; i < codes; i++) {
+            codeString[i] = "";
+        }
+        int code = 0;
+        int position = 0;
+        char ch = '{';
+        while (ch != '}') { //stop if end of list
+            ch = inputString.charAt(position);
+            if (Character.isDigit(ch)) {
+                while (Character.isDigit(ch)) {
+                    codeString[code] += ch;
+                    position++;
+                    ch = inputString.charAt(position);
+                }
+                code++; //move to next code string
+            }
+            position++; //point to next character
+        }
+        return codeString;
+    }
 }
+
 
 
 
